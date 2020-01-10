22d5e4777932344346da9879f08d3663f1ef9f9a
George Sowles <georgesowles@mb-georges1.campus.steds.org>
2020-01-10 15:17:59 -0500

    Quadratic calculator introduced MACBOOK

---
+++ QuadraticCalculator.js
@@ -0,0 +1,14 @@
+
+//ax^2+bx+c
+
+
+var a=2
+var b=2
+var c=-12
+Quadraticformula1 = ((-1*b)+(Math.sqrt((Math.pow(b,2)) - (4*a*c)))/(2*a))
+Quadraticformula2 = ((-1*b)-(Math.sqrt((Math.pow(b,2)) - (4*a*c)))/(2*a))
+print("x =")
+print (Quadraticformula1)
+print("")
+print("x =")
+print (Quadraticformula2)
