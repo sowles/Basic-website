41d984dbd05d16e24ad2f51e120692e8db8522f0
BuildTools <unconfigured@null.spigotmc.org>
2020-01-11 11:05:21 -0500

    Examgrade calculator added

---
+++ ExamGradeCalculator.js
@@ -0,0 +1,15 @@
+//place holder variables
+
+//R = required Grade
+//G = Goal
+//C = Current Grade
+//FW = Final Weight
+
+//var R
+var G = 82
+var C = 76
+var FW = 4
+
+//Equation
+
+R = ((G-C)*(100-FW))/FW
