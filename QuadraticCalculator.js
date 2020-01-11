4183f975de6a99ea66e123ef49e4af716eb90408
George Sowles <georgesowles@mb-georges1.local>
2020-01-11 18:59:13 -0500

    counflip redo MACBOOK

--- QuadraticCalculator.js
+++ QuadraticCalculator.js
@@ -7,8 +7,8 @@ var b=2
 var c=-12
 Quadraticformula1 = ((-1*b)+(Math.sqrt((Math.pow(b,2)) - (4*a*c)))/(2*a))
 Quadraticformula2 = ((-1*b)-(Math.sqrt((Math.pow(b,2)) - (4*a*c)))/(2*a))
-print("x =")
-print (Quadraticformula1)
-print("")
-print("x =")
-print (Quadraticformula2)
+//print("x =")
+//print (Quadraticformula1)
+//print("")
+("x =")
+ (Quadraticformula2)
