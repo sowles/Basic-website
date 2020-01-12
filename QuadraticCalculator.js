028c939a2a3c39c97eb754158ae3b90a87cc914a
BuildTools <unconfigured@null.spigotmc.org>
2020-01-11 23:38:22 -0500

    test

--- QuadraticCalculator.js
+++ QuadraticCalculator.js
@@ -9,8 +9,8 @@
      var a = document.getElementById("QuadFormA").value;
      var b = document.getElementById("QuadFormB").value;
      var c = document.getElementById("QuadFormC").value;
-
-     document.getElementById("result1").textContent = ((-1*b)+(Math.sqrt((Math.pow(b,2)) - (4*a*c)))/(2*a));
-     document.getElementById("result2").textContent = ((-1*b)-(Math.sqrt((Math.pow(b,2)) - (4*a*c)))/(2*a));
+document.getElementById("result1").textContent = a+b+c;
+    // document.getElementById("result1").textContent = ((-1*b)+(Math.sqrt((Math.pow(b,2)) - (4*a*c)))/(2*a));
+    // document.getElementById("result2").textContent = ((-1*b)-(Math.sqrt((Math.pow(b,2)) - (4*a*c)))/(2*a));
    });
  });
