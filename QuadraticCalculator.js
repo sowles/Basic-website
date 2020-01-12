c39c37761368ea0ec8a39e4644156d0ea4a3ebdd
BuildTools <unconfigured@null.spigotmc.org>
2020-01-11 23:20:31 -0500

    quadfor update

--- QuadraticCalculator.js
+++ QuadraticCalculator.js
@@ -9,8 +9,6 @@
      var a = document.getElementById("QuadFormA").value;
      var b = document.getElementById("QuadFormB").value;
      var c = .01 * document.getElementById("QuadFormC").value;
-     var Quadraticformula1 = ((-1*b)+(Math.sqrt((Math.pow(b,2)) - (4*a*c)))/(2*a));
-     var Quadraticformula2 = ((-1*b)-(Math.sqrt((Math.pow(b,2)) - (4*a*c)))/(2*a));
 
      document.getElementById("result").textContent = ((-1*b)+(Math.sqrt((Math.pow(b,2)) - (4*a*c)))/(2*a)); + ((-1*b)-(Math.sqrt((Math.pow(b,2)) - (4*a*c)))/(2*a));
 
