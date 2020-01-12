56abf3d16da0d449f39921afdaacab31eb74854c
George Sowles <georgesowles@mb-georges1.local>
2020-01-11 20:49:26 -0500

    warm up the calculator. send it good vibes.

--- ExamGradeCalculator.js
+++ ExamGradeCalculator.js
@@ -12,8 +12,8 @@ document.addEventListener("DOMContentLoaded", function() {
 
   document.getElementById("submitcalculations").addEventListener("click", function() {
     var result;
-    var goal = document.getElementById("currentGrade").value;
-    var currentGrade = document.getElementById("wantedgrade").value;
+    var currentGrade = document.getElementById("currentGrade").value;
+    var goal = document.getElementById("wantedgrade").value;
     var finalweight = .01 *document.getElementById("finalweight").value;
     document.getElementById("result").textContent = (goal - currentGrade * (1 - finalweight)) / finalweight;
 
