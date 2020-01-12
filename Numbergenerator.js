d926134e9159767c92a8d299c00c4dc9e987e5ad
George Sowles <georgesowles@mb-georges1.local>
2020-01-11 19:43:29 -0500

    f

--- Numbergenerator.js
+++ Numbergenerator.js
@@ -4,5 +4,5 @@ function getRandomInt(max) {
 
 const maxnum = document.getElementById("inputMaxnum");
 maxnum.addEventListener("keyup", function() {
-  document.getElementById("rannum").textContent = getRandomInt(maxnum.value);
+  document.getElementById("rannum").textContent = "Random Number between 0 and " + maxmun + ": " + getRandomInt(maxnum.value);
 });
