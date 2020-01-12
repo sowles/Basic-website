99133bbfdfc1b629f7162fde9c2a97c15944be45
George Sowles <georgesowles@mb-georges1.local>
2020-01-11 20:08:18 -0500

    fqwe

--- ExamGradeCalculator.js
+++ ExamGradeCalculator.js
@@ -7,18 +7,15 @@
 
 //var R
 
-
-var result;
-var goal = document.getElementById("currentGrade").value;
-var currentGrade = document.getElementById("wantedgrade").value;
-var finalweight = document.getElementById("finalweight").value;
-
-
-
 //Equation
 document.addEventListener("DOMContentLoaded", function() {
+  var result;
+  var goal = document.getElementById("currentGrade").value;
+  var currentGrade = document.getElementById("wantedgrade").value;
+  var finalweight = document.getElementById("finalweight").value;
+
   document.getElementById("submitcalculations").addEventListener("click", function() {
-    document.getElementById("result").textContent = goal;
+    document.getElementById("result").textContent = ((goal-currentGrade)*(100-finalweight))/finalweight;
 
   });
 });
