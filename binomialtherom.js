71f921a10b3567cb993369d3a962271c7915b408
BuildTools <unconfigured@null.spigotmc.org>
2020-01-12 10:57:47 -0500

    s

--- binomialtherom.js
+++ binomialtherom.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
     function binomial(n, k) {
      if ((typeof n !== 'number') || (typeof k !== 'number'))
   return false;
-    var coeff = document.getElementById("N").value;;
+    var coeff = 1;
     for (var x = n-k+1; x <= n; x++) coeff *= x;
     for (x = 1; x <= k; x++) coeff /= x;
     return coeff;
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", function() {
 console.log(binomial(8,3));
 console.log(binomial(10,2));
 
-    document.getElementById("result").textContent = coeff ;
+    document.getElementById("result").textContent = (binomial(document.getElementById("N").value,document.getElementById("K").value)) ;
 
   });
 });
