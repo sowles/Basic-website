cd464f559eb799f8dcfdc6929911be67b727c575
George Sowles <georgesowles@mb-georges1.local>
2020-01-11 21:13:29 -0500

    frgthj

--- ExamGradeCalculator.js
+++ ExamGradeCalculator.js
@@ -14,7 +14,7 @@ document.addEventListener("DOMContentLoaded", function() {
     var result;
     var currentGrade = document.getElementById("currentGrade").value;
     var goal = document.getElementById("wantedgrade").value;
-    var finalweight = .01 *document.getElementById("finalweight").value;
+    var finalweight = .01 * document.getElementById("finalweight").value;
     document.getElementById("result").textContent = (goal - wantedgrade * (1 - finalweight)) / finalweight;
 
   });
