65d300ed274c576203f80352923aeb181a3b1f89
BuildTools <unconfigured@null.spigotmc.org>
2020-01-11 23:18:27 -0500

    quadform update

--- QuadraticCalculator.js
+++ QuadraticCalculator.js
@@ -2,13 +2,17 @@
 //ax^2+bx+c
 
 
-var a=2
-var b=2
-var c=-12
-Quadraticformula1 = ((-1*b)+(Math.sqrt((Math.pow(b,2)) - (4*a*c)))/(2*a))
-Quadraticformula2 = ((-1*b)-(Math.sqrt((Math.pow(b,2)) - (4*a*c)))/(2*a))
-//print("x =")
-//print (Quadraticformula1)
-//print("")
-//("x =")
- //(Quadraticformula2)
+ document.addEventListener("DOMContentLoaded", function() {
+
+   document.getElementById("submitcalculations").addEventListener("click", function() {
+
+     var a = document.getElementById("QuadFormA").value;
+     var b = document.getElementById("QuadFormB").value;
+     var c = .01 * document.getElementById("QuadFormC").value;
+     var Quadraticformula1 = ((-1*b)+(Math.sqrt((Math.pow(b,2)) - (4*a*c)))/(2*a));
+     var Quadraticformula2 = ((-1*b)-(Math.sqrt((Math.pow(b,2)) - (4*a*c)))/(2*a));
+
+     document.getElementById("result").textContent = ((-1*b)+(Math.sqrt((Math.pow(b,2)) - (4*a*c)))/(2*a)); + ((-1*b)-(Math.sqrt((Math.pow(b,2)) - (4*a*c)))/(2*a));
+
+   });
+ });
