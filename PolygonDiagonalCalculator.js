1389a623e5a992d2388e03ee022d9e88d138d2d8
BuildTools <unconfigured@null.spigotmc.org>
2020-01-27 16:23:55 -0500

    unknown change

---
+++ PolygonDiagonalCalculator.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+  document.getElementById("submitcalculations").addEventListener("click", function() {
+
+    document.getElementById("polyn").textContent*(document.getElementById("polyn").textContent-3)/2
+
+  });
+});
