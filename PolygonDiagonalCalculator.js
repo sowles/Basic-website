a472f149f2f1aba7c69df57bc4a9e1238aea24c5
BuildTools <unconfigured@null.spigotmc.org>
2020-01-27 20:13:08 -0500

    changed to .value

--- PolygonDiagonalCalculator.js
+++ PolygonDiagonalCalculator.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
   document.getElementById("submitcalculations").addEventListener("click", function() {
     var result;
-    var sides = document.getElementById("polyn").textContent;
+    var sides = document.getElementById("polyn").value;
     document.getElementById("result").textContent = sides*(sides-3)/2;
 
   });
