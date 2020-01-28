96d9da0855d57f6930b1f0363bd97e680f5d5afe
George Sowles <georgesowles@mb-georges1.campus.steds.org>
2020-01-28 10:19:14 -0500

    introduction of orderedlistcalculator

---
+++ orderedlistcalculator.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+  document.getElementById("submitcalculations").addEventListener("click", function() {
+    var result;
+    var numofelements = document.getElementById("elementinput");
+    var maxnum = document.getElementById("digitinput");
+    var evenconstraint = document.getElementById("constraintinput");
+    document.getElementById("result").textContent = (maxnum(Math.pow(b,numofelements))-(constraintinput(Math.pow(b,numofelements))));
+
+  });
+  });
