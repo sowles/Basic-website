68738aca46805d7e45804cdcfa2fa0fea6a61772
BuildTools <unconfigured@null.spigotmc.org>
2020-01-27 20:10:46 -0500

    sides and result variables introduced

--- PolygonDiagonalCalculator.js
+++ PolygonDiagonalCalculator.js
@@ -1,8 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
 
   document.getElementById("submitcalculations").addEventListener("click", function() {
-
-    document.getElementById("result").textContent = document.getElementById("polyn").textContent*(document.getElementById("polyn").textContent-3)/2
+    var result;
+    var sides = document.getElementById("polyn").textContent;
+    document.getElementById("result").textContent = sides*(sides-3)/2
 
   });
   });
