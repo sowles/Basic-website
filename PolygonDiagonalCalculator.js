f4c13ee2ee44361cae0af17746bfc3fdc4599770
BuildTools <unconfigured@null.spigotmc.org>
2020-01-27 20:12:05 -0500

    ; added

--- PolygonDiagonalCalculator.js
+++ PolygonDiagonalCalculator.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
   document.getElementById("submitcalculations").addEventListener("click", function() {
     var result;
     var sides = document.getElementById("polyn").textContent;
-    document.getElementById("result").textContent = sides*(sides-3)/2
+    document.getElementById("result").textContent = sides*(sides-3)/2;
 
   });
   });
