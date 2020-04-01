a149de291bca70bccb81d04a1c275fd788c6bd06
George Sowles <georgesowles@GeorgeMacbook.westell.com>
2020-03-31 22:09:06 -0400

    added event listener

--- matrix.js
+++ matrix.js
@@ -1,3 +1,7 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+
+  document.getElementById("calculateMatrixButton").addEventListener("click", function() {
 let matrix1 = new Array(
                         [value.document.getElementById("matrixa11"), value.document.getElementByName("matrixa12"), value.document.getElementByName("matrixa13")],
                         [value.document.getElementById("matrixa21"), value.document.getElementById("matrixa22"), value.document.getElementById("matrixa23")],
@@ -35,3 +39,4 @@ function multmatrix(matrix1, matrix2){
 }
 matrixFinal = document.getElementById("multresult").textContent;
 console.log(multmatrix(matrix1, matrix2).toString());
+}
