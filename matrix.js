a0a2976ede6594fc119046c4b575c088e9e03f44
George Sowles <georgesowles@GeorgeMacbook.westell.com>
2020-04-01 11:23:18 -0400

    by name removed

--- matrix.js
+++ matrix.js
@@ -3,13 +3,13 @@ document.addEventListener("DOMContentLoaded", function() {
 
   document.getElementById("calculateMatrixButton").addEventListener("click", function() {
 let matrix1 = new Array(
-                        [value.document.getElementById("matrixa11"), value.document.getElementByName("matrixa12"), value.document.getElementByName("matrixa13")],
+                        [value.document.getElementById("matrixa11"), value.document.getElementById("matrixa12"), value.document.getElementById("matrixa13")],
                         [value.document.getElementById("matrixa21"), value.document.getElementById("matrixa22"), value.document.getElementById("matrixa23")],
                         [value.document.getElementById("matrixa31"), value.document.getElementById("matrixa32"), value.document.getElementById("matrixa33")]
 )
 
 let matrix2 = new Array(
-                        [value.document.getElementById("matrixb11"), value.document.getElementByName("matrixb12"), value.document.getElementByName("matrixb13")],
+                        [value.document.getElementById("matrixb11"), value.document.getElementById("matrixb12"), value.document.getElementById("matrixb13")],
                         [value.document.getElementById("matrixb21"), value.document.getElementById("matrixb22"), value.document.getElementById("matrixb23")],
                         [value.document.getElementById("matrixb31"), value.document.getElementById("matrixb32"), value.document.getElementById("matrixb33")]
 )
