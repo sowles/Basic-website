fa6cd47829301a687f8cc30d6c62a88b23539c68
George Sowles <georgesowles@GeorgeMacbook.westell.com>
2020-04-02 17:41:24 -0400

    jhg

--- matrix.js
+++ matrix.js
@@ -29,7 +29,7 @@ document.addEventListener("DOMContentLoaded", function() {
     let matrix1 = new Array();
     let matrix2 = new Array();
     let counter = 0;
-    for (row in document.querySelectorAll('#matrix1Form > section')) {
+    for (row in document.querySelectorAll('#matrix1Form section')) {
       counter ++;
       let rowArray = new Array();
       for (item in document.querySelectorAll('#matrix1Form > section')[row].children) {
@@ -38,9 +38,9 @@ document.addEventListener("DOMContentLoaded", function() {
       matrix1.push(rowArray);
     }
 
-    for (row in document.querySelectorAll('#matrix2Form > section')) {
+    for (row in document.querySelectorAll('#matrix2Form section')) {
       let rowArray = new Array();
-      for (item in document.querySelectorAll('#matrix2Form > section')[row].children) {
+      for (item in document.querySelectorAll('#matrix2Form section')[row].children) {
         rowArray.push(parseInt(document.querySelectorAll('#matrix2Form > *')[row].children[item].value));
       }
       matrix2.push(rowArray);
