ec04edf38bbd6c186808553a73aa0997d49eac43
George Sowles <georgesowles@GeorgeMacbook.local>
2020-04-22 09:06:18 -0400

    permchange

--- Calculators/calcjs/permcalc.js
+++ Calculators/calcjs/permcalc.js
@@ -5,15 +5,15 @@ document.addEventListener("DOMContentLoaded", function() {
     var n = document.getElementById("permn").value;
     var r = document.getElementById("permr").value;
     var factorial = function(int){
-      if (int === 0){
-        return 1
-      }else if (int < 0){
-        alert("That is a negative number")
-        return;
-      }else{
+      //if (int === 0){
+        //return 1
+      //}else if (int < 0){
+        //alert("That is a negative number")
+    //    return;
+    //  }else{
         int = int*factorial(int-1);
         return(int);
-      }
+      //}
     };
     document.getElementById("result").textContent = (factorial(n))/(factorial(n-r));
 
