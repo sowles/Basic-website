adf59a37efdaa129b5685e5d32cba96acf372952
George Sowles <georgesowles@GeorgeMacbook.westell.com>
2020-05-04 19:13:11 -0400

    s

--- Calculators/calcjs/permcalc.js
+++ Calculators/calcjs/permcalc.js
@@ -1,28 +1,30 @@
+function factorial(int){
+  if (int > 0) {
+    int = int*factorial(int-1);
+    console.log(1)
+    return(int);
+  }
+  else if (int == 0) {
+    return 1;
+  }
+  else {
+    return 0;
+  }
+};
+
+
 document.addEventListener("DOMContentLoaded", function() {
 
   document.getElementById("submitcalculations").addEventListener("click", function() {
-    let invalidResult = false;
     var result;
     var n = document.getElementById("permn").value;
     var r = document.getElementById("permr").value;
-    var factorial = function(int){
-      if (int > 0){
-        int = int*factorial(int-1);
-        console.log(1)
-        return(int);
-      }
-      else if (int == 0) {
-        return 1;
-      }
-      else {
-        invalidResult = true;
-      }
-    };
-    if (invalidResult) {
+
+    if (n < 0 || (n-r < 0)) {
       document.getElementById("result").textContent = "Yo, you're negative dawg";
     }
     else {
       document.getElementById("result").textContent = (factorial(n))/(factorial(n-r));
     }
   });
-  });
+});
