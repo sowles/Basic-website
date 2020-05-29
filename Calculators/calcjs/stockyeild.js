f271742004ad7ef1a017dbb1a5b068fad2e14afe
George Sowles <georgesowles@GeorgeMacbook.local>
2020-05-29 19:30:09 -0400

    stockyeild

---
+++ Calculators/calcjs/stockyeild.js
@@ -0,0 +1,12 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+
+  document.getElementById("submitcalculations").addEventListener("click", function() {
+    var result;
+    var initial = document.getElementById("initial").value;
+    var ending = document.getElementById("ending").value;
+    var dividends = document.getElementById("dividends").value;
+    document.getElementById("result").textContent = (initial-ending+dividends)/initial;
+
+  });
+});
