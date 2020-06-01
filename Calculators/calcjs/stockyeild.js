b6c8e646b35cef87d48d79ea91fd8085eb955fb9
George Sowles <georgesowles@GeorgeMacbook.local>
2020-06-01 19:01:10 -0400

    multchange

--- Calculators/calcjs/stockyeild.js
+++ Calculators/calcjs/stockyeild.js
@@ -3,6 +3,8 @@ document.addEventListener("DOMContentLoaded", function() {
     const initial = parseInt(document.getElementById("initial").value);
     const ending = parseInt(document.getElementById("ending").value);
     const dividends = parseInt(document.getElementById("dividends").value);
-    document.getElementById("result").textContent = "$" + ((ending - initial) + dividends);
+    const sharesBought = parseInt(document.getElementById("sharesBought").value);
+    const sharesSold = parseInt(document.getElementById("sharesBought").value);
+    document.getElementById("result").textContent = "Total yield: $" + (((ending * sharesSold) - (initial * sharesBought)) + dividends);
   });
 });
