bbfa4ca63775fd4d89ae36fa099db783c9bb1f22
George Sowles <georgesowles@georgemacbook.campus.steds.org>
2020-09-14 10:40:50 -0400

    jdsf

--- Calculators/calcjs/distanceForm.js
+++ Calculators/calcjs/distanceForm.js
@@ -7,8 +7,8 @@ document.addEventListener("DOMContentLoaded", function() {
     var x3 = document.getElementById("y1").value;
     var x4 = document.getElementById("y2").value;
     // var formResult = (Math.sqrt(Math.pow((x1 - x2),2) + (Math.pow((y1 - y2),2))));
-    var formResult = Math.sqrt(4);
-    console.log(formResult) 
+    var formResult = Math.sqrt(x1);
+    console.log(formResult)
 
 //document.getElementById("result1").textContent = a+b+c;
 
